e829a3f814b99a8d53251024fcad4a239e4d47be
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-16 17:03:23 -1000

    post will add body data to json file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var jade = require('jade');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var app = express();
 
@@ -17,15 +18,19 @@ app.route('/')
 app.route('/elements')
   .post(
     validateElement,
-    pageBuild //,
-    // res.render()
+    addElementToData
   );
 
 
 // use the request params to full the jade template
 app.get('/:elementName', function (req, res) {
   console.log(req.params);
-  res.render('element', {elementName: req.params.elementName, elementSymbol: req.params.elementSymbol, elementAtomicNumber: req.params.elementAtomicNumber, elementDescription: req.params.elementDescription});
+  res.render('element', {
+    elementName: req.params.elementName,
+    elementSymbol: req.params.elementSymbol,
+    elementAtomicNumber: req.params.elementAtomicNumber,
+    elementDescription: req.params.elementDescription
+  });
 });
 
 function validateElement(req, res, next) {
@@ -38,24 +43,35 @@ function validateElement(req, res, next) {
 }
 
 // alter this function so that it adds the req info to data.json
-function pageBuild(req, res, next) {
-  var newPage = '<html lang="en">' +
-      '<head>' +
-        '<meta charset="UTF-8">' +
-        '<title> The Elements -' + req.body.elementName + '</title>' +
-        '<link rel="stylesheet" href="/css/styles.css">' +
-      '</head>' +
-      '<body>' +
-        '<h1>' + req.body.elementName + '</h1>' +
-        '<h2>' + req.body.elementSymbol + '</h2>' +
-        '<h3>' + req.body.elementAtomicNumber + '</h3>' +
-        '<p>' + req.body.elementDescription + '</p>' +
-        '<p><a href="/">back</a></p>' +
-      '</body>' +
-    '</html>';
-  fs.writeFile('./public/' + req.body.elementName + '.html', newPage);
+function createElementData(req, res, next) {
+  var newElement = {};
+  newElement[req.body.elementName] =
+    {
+      "elementName": req.body.elementName,
+      "elementSymbol": req.body.elementSymbol,
+      "elementAtomicNumber": req.body.elementAtomicNumber,
+      "elementDescription": req.body.elementDescription
+    };
+
+  return newElement;
+}
+
+function addElementToData(req, res, next) {
+  var newElement = createElementData(req);
+  console.log('writing to file...');
+  //use synchronous function???
+  // var originalFile = fs.readFileSync('data.json');
+  // var original = JSON.parse(originalFile);
+  // console.log(original);
+  // original.push(newElement);
+  // var originalJSON = JSON.stringify(original);
+  fs.writeFile('data.json', JSON.stringify(newElement), function (err) {
+    if (err) throw err;
+  });
+  res.json(newElement);
   return next();
 }
+
 
 var server = app.listen(3000, function ()  {
   var port = server.address().port;
